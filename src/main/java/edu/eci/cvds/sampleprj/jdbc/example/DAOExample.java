b51eba43518ea727a43ab2095b4905264a18dd9b
jsebasg <juan.garcia-h@mail.escuelaing.edu.co>
2021-03-20 09:28:17 -0500

    parte 2 iinicio

--- src/main/java/edu/eci/cvds/sampleprj/jdbc/example/DAOExample.java
+++ src/main/java/edu/eci/cvds/sampleprj/jdbc/example/DAOExample.java
@@ -1,8 +1,10 @@
 package edu.eci.cvds.sampleprj.jdbc.example;
 
+import edu.eci.cvds.samples.services.ServiciosAlquilerFactory;
+
 public class DAOExample {
 
     public static void main(String args[]) {
-
+        ServiciosAlquilerFactory(); 
     }
 }
